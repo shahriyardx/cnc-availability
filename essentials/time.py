16d570f173c1d76ec8675badad2e210470ee0f29
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2023-03-18 02:24:26 +0600

    Fix stuff

--- essentials/time.py
+++ essentials/time.py
@@ -1,41 +1,25 @@
 from datetime import datetime, timedelta
-from typing import Optional, Tuple, Union
-
 from typing_extensions import Literal
-
-TIME = Union[datetime, Tuple[datetime, str, Tuple[int, int, int]]]
 
 Days = Literal[
     "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
 ]
 
-day_map = {
-    "Monday": 0,
-    "Tuesday": 1,
-    "Wednesday": 2,
-    "Thursday": 3,
-    "Friday": 4,
-    "Saturday": 5,
-    "Sunday": 6,
-}
+def get_next_date(day: Days, hour=0, minute=0, second=0):
+    # Convert day name to integer representation
+    day_dict = {'monday': 0, 'tuesday': 1, 'wednesday': 2, 'thursday': 3, 'friday': 4, 'saturday': 5, 'sunday': 6}
+    day_num = day_dict.get(day.lower())
 
+    # Get current date and time
+    now = datetime.utcnow()
 
-def get_next_date(
-    day: Days,
-    hour: int = 17,
-    minute: int = 0,
-    second: int = 0,
-    microsecond: int = 0,
-    default_now: Optional[datetime] = None,
-):
-    now = default_now or datetime.utcnow()
+    # Calculate the datetime for the specified day and time
+    target_time = now.replace(hour=hour, minute=minute, second=second, microsecond=0)
+    target_day = (day_num - now.weekday()) % 7
+    target_datetime = target_time + timedelta(days=target_day)
 
-    days_left = day_map[day] - now.weekday()
-    if days_left <= 0:  # Target day already happened this week
-        days_left += 7
+    # Check if the target datetime has already passed
+    if target_datetime < now:
+        target_datetime += timedelta(weeks=1)
 
-    next_date = now.replace(
-        hour=hour, minute=minute, second=second, microsecond=microsecond
-    ) + timedelta(days=days_left)
-
-    return next_date
+    return target_datetime
