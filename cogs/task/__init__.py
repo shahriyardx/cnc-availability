592010da33405201283a924b5c84eb36491979a5
Md Shahriyar Alam <contact@shahriyar.dev>
2023-12-12 03:04:50 +0600

    remove sleep

--- cogs/task/__init__.py
+++ cogs/task/__init__.py
@@ -283,7 +283,7 @@ class Tasker(commands.Cog):
                 if total_avails < 3:
                     await append_into_ir(self.bot, guild, member, self.roster_sheet, 0)
 
-                await asyncio.sleep(60)
+                await asyncio.sleep(5)
 
         # Member count check
         for guild in self.bot.guilds:
