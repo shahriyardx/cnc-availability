e056ee6201536fcabf8cb8fdeccef142025b7bc7
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2024-04-05 04:05:15 +0600

    fix weekday

--- cogs/commands/admin.py
+++ cogs/commands/admin.py
@@ -25,15 +25,28 @@ from datetime import datetime
 from pytz import timezone
 
 
-def can_submit():
+def can_submit(day):
     now = datetime.now(tz=timezone("EST"))
     weekday = now.weekday()
+
+    days = {
+        "Monday": 0,
+        "Tuesday": 1,
+        "Wednesday": 2,
+        "Thursday": 3,
+        "Friday": 4,
+        "Saturday": 5,
+        "Sunday": 6,
+    }
 
     if weekday == 0:
         return True
 
     if weekday > 3:
         return None
+
+    if weekday != days[day]:
+        return False
 
     last_time = datetime(
         now.year, now.month, now.day, 20, 30, 0, 0, tzinfo=timezone("EST")
@@ -503,7 +516,7 @@ class UtilityCommands(commands.Cog):
     ):
         await interaction.response.defer(ephemeral=True)
 
-        can = can_submit()
+        can = can_submit(day)
 
         if can is None:
             return await interaction.followup.send(
